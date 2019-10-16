ba6d44ee6cb178743478a73b80cf3f62d1970939
tfsn <tfsn86@gmail.com>
2019-10-16 09:58:56 +0200

    Created components for the site

---
+++ src/components/PhotoContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PhotoContainer extends Component {
+	state = {};
+
+	render() {
+		return <h1>Test</h1>;
+	}
+}
---
+++ src/components/SearchForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SearchForm extends Component {
+	state = {};
+
+	render() {
+		return <h1>Test</h1>;
+	}
+}
