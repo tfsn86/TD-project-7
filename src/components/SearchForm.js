913e16c757334b70932b09525e33f197ac2c215c
tfsn <tfsn86@gmail.com>
2019-10-21 21:10:05 +0200

    Update SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 export default class SearchForm extends Component {
-	state = {};
+	state = {
+		searctText: ''
+	};
+
+	onSearchChange = e => {
+		this.setState({ searctText: e.target.value });
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		this.props.onSearch(this.query.value);
+		e.currentTarget.reset();
+	};
 
 	render() {
 		return (
-			<form className="search-form">
-				<input type="search" name="search" placeholder="Search" required />
+			<form className="search-form" onSubmit={this.handleSubmit}>
+				<input
+					type="search"
+					onChange={this.onSearchChange}
+					name="search"
+					ref={input => (this.query = input)}
+					placeholder="Search..."
+					required
+				/>
 				<button type="submit" className="search-button">
 					<svg
 						fill="#fff"
