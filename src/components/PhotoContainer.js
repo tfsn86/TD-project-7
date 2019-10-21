114f583b5c104f78acbf9e3eaad42223701866b1
tfsn <tfsn86@gmail.com>
2019-10-21 21:10:08 +0200

    Update PhotoContainer.js

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import NotFound from './NotFound';
+import React from 'react';
 import Photo from './Photo';
 
-export default class PhotoContainer extends Component {
-	state = {};
+const PhotoContainer = props => {
+	const results = props.data;
+	let photos = results.map(photo => (
+		<Photo
+			key={photo.id}
+			url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+			alt={photo.title}
+		/>
+	));
 
-	render() {
-		return (
-			<div className="photo-container">
-				<h2>Results</h2>
-				<ul>
-					<Photo />
-					<NotFound />
-				</ul>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="photo-container">
+			<h2>Results</h2>
+			<ul>{photos}</ul>
+		</div>
+	);
+};
+
+export default PhotoContainer;
