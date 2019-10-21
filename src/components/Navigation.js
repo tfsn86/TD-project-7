67a34d10b59f7758b5deb94f8e999021a33ab6b3
tfsn <tfsn86@gmail.com>
2019-10-21 21:10:15 +0200

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Navigation = () => {
-	return (
-		<nav className="main-nav">
-			<ul>
-				<li>
-					<a href="#">Cats</a>
-				</li>
-				<li>
-					<a href="#">Dogs</a>
-				</li>
-				<li>
-					<a href="#">Computers</a>
-				</li>
-			</ul>
-		</nav>
-	);
-};
+const Navigation = () => (
+	<nav className="main-nav">
+		<ul>
+			<li>
+				<NavLink to={`./cats`}>Cats</NavLink>
+			</li>
+			<li>
+				<NavLink to={`/dogs`}>Dogs</NavLink>
+			</li>
+			<li>
+				<NavLink to={`/lions`}>Lions</NavLink>
+			</li>
+		</ul>
+	</nav>
+);
 
 export default Navigation;
