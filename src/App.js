41383523b2bd9a5f6c5efe6dcad20373b7d0d182
tfsn <tfsn86@gmail.com>
2019-10-22 19:52:11 +0200

    Routes for navigation links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import apiKEY from './config.js';
 import axios from 'axios';
 
@@ -12,15 +12,21 @@ export default class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			photos: []
+			photos: [],
+			dogs: [],
+			cats: [],
+			lions: []
 		};
 	}
 
 	componentDidMount = () => {
 		this.performSearch();
+		this.dogsSearch();
+		this.catsSearch();
+		this.lionsSearch();
 	};
 
-	performSearch = (query = 'cats') => {
+	performSearch = query => {
 		axios
 			.get(
 				`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKEY}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
@@ -38,13 +44,93 @@ export default class App extends Component {
     }) */
 	};
 
+	dogsSearch = () => {
+		axios
+			.get(
+				`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKEY}&tags=dogs&per_page=24&format=json&nojsoncallback=1`
+			)
+			.then(response => {
+				this.setState({ dogs: response.data.photos.photo });
+				// handle success
+			})
+			.catch(error => {
+				// handle error
+				console.log('Error fetching and parsing data', error);
+			});
+		/* .finally(function() {
+    // always executed
+  }) */
+	};
+
+	catsSearch = () => {
+		axios
+			.get(
+				`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKEY}&tags=cats&per_page=24&format=json&nojsoncallback=1`
+			)
+			.then(response => {
+				this.setState({ cats: response.data.photos.photo });
+				// handle success
+			})
+			.catch(error => {
+				// handle error
+				console.log('Error fetching and parsing data', error);
+			});
+		/* .finally(function() {
+    // always executed
+  }) */
+	};
+
+	lionsSearch = () => {
+		axios
+			.get(
+				`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKEY}&tags=lions&per_page=24&format=json&nojsoncallback=1`
+			)
+			.then(response => {
+				this.setState({ lions: response.data.photos.photo });
+				// handle success
+			})
+			.catch(error => {
+				// handle error
+				console.log('Error fetching and parsing data', error);
+			});
+		/* .finally(function() {
+    // always executed
+  }) */
+	};
+
 	render() {
 		return (
 			<Router>
 				<div className="container">
-					<Navigation />
 					<SearchForm onSearch={this.performSearch} />
-					<PhotoContainer data={this.state.photos} />
+					<Navigation />
+					<Switch>
+						<Route
+							exact
+							path="/"
+							render={() => (
+								<PhotoContainer data={this.state.photos} title="Results" />
+							)}
+						/>
+						<Route
+							path="/dogs"
+							render={() => (
+								<PhotoContainer data={this.state.dogs} title="Dogs" />
+							)}
+						/>
+						<Route
+							path="/cats"
+							render={() => (
+								<PhotoContainer data={this.state.cats} title="Cats" />
+							)}
+						/>
+						<Route
+							path="/lions"
+							render={() => (
+								<PhotoContainer data={this.state.lions} title="Lions" />
+							)}
+						/>
+					</Switch>
 				</div>
 			</Router>
 		);
