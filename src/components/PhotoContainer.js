3546931fa25099a1850434bfbfb46504e29d4695
tfsn <tfsn86@gmail.com>
2019-10-22 20:22:55 +0200

    Implemented the component with page not found

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Photo from './Photo';
+import NotFound from './NotFound';
 
 const PhotoContainer = props => {
 	const results = props.data;
-	let photos = results.map(photo => (
-		<Photo
-			key={photo.id}
-			url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-			alt={photo.title}
-		/>
-	));
+	let photos;
+	if (results.length > 0) {
+		photos = results.map(photo => (
+			<Photo
+				key={photo.id}
+				url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+				alt={photo.title}
+			/>
+		));
+	} else {
+		photos = <NotFound />;
+	}
 
 	return (
 		<div className="photo-container">
